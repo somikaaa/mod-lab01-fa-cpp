0dffd24b006f27f4bd5041edfc6b5096598934ed
somikaaa <124293047+somikaaa@users.noreply.github.com>
2024-04-25 19:14:13 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
-// Copyright 2022 UNN-IASR
-#include "fun.h"
+#include <iostream>
+#include "lab1.h"
 
 int main() {
+    const char* text = "Hello World! ";
+    std::cout << "faStr1: " << faStr1(text) << std::endl;
+    std::cout << "faStr2: " << faStr2(text) << std::endl;
+    std::cout << "faStr3: " << faStr3(text) << std::endl;
     return 0;
 }
-//ку
