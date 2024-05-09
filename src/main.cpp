5a8a75bd24c7143a83de70f3fe6345531a9d607d
somikaaa <124293047+somikaaa@users.noreply.github.com>
2024-05-09 19:39:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,19 @@
+// Copyright 2022 UNN-IASR
 #include <iostream>
 #include "fun.h"
 
 int main() {
-
     const char* str1 = "qwe4  gftt e 555gr rrgr  grhh ";
     const char* str2 = "  qwe4 ";
-
     const char* str3 = "Hello, world!";
     const char* str4 = "Hello world!";
-
     const char* str5 = " bad dog ";
     const char* str6 = " six monkeys";
-
-
     std::cout << "FA1: " << faStr1(str1) << std::endl;
     std::cout << "FA1: " << faStr1(str2) << std::endl;
-
     std::cout << "FA2: " << faStr2(str3) << std::endl;
     std::cout << "FA2: " << faStr2(str4) << std::endl;
-
     std::cout << "FA3: " << faStr3(str5) << std::endl;
     std::cout << "FA3: " << faStr3(str6) << std::endl;
-
     return 0;
 }
